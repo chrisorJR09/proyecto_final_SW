f41ac5ff9da286437b3385a1b0efa904e7bd1f7e
Chris <chris.jauran1@gmail.com>
2025-11-14 09:16:38 -0600

    Se crea archivo server.js y se suben archivos de package.json

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-const express=require("express");
+//configuración del servidor
+const express = require("express");
+const cors = require("cors");
+
+const app=express();
+
+app.use(cors());
+
+//middlewares
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+//configuración de arranque del servidor
+const PORT = process.env.PORT || 3000;
+
+//Ruta en raíz, para probar el funcionamiento del API
+app.get("/", (req, res)=>{
+    console.log("Servidor funcionando correctamente");
+    res.send("API funcionando correctamente");
+})
+
+//Funcion para escuchar al puerto
+app.listen(PORT, ()=>{
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+})

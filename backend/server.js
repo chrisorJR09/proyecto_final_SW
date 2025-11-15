223b5ad51b02239964f524129889247a38a3b6de
Alexayalee <alexa05sanchez@gmail.com>
2025-11-15 06:31:23 -0600

    Agrego módulos de gráfico al frontend y backend

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+// server.js 
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const graficaRoutes = require('./routes/graficaRoutes');
+const pool = require('./db/conexion'); // <-- Importamos la conexión
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Ruta base
+app.get('/', (req, res) => {
+    res.send('API Ventas de Alexa Yalee Sanchez Tejeda funcionando correctamente');
+});
+
+// Rutas principales
+app.use('/api/ventas_producto', graficaRoutes);
+
+// Funcion que hace una consulta de prueba mínima que
+// confirma que todo el circuito conexión => consulta => respuesta está funcionando
+async function testConnection() {
+    try {
+        const [rows] = await pool.query('SELECT 1 + 1 AS result'); // Le pide a MySQL que sume 1 + 1, y le ponga el alias result al valor
+        console.log('✅ Conexión a la base de datos establecida. Resultado, Alexa Yalee Sanchez Tejeda:', rows[0].result);
+    } catch (error) {
+        console.error('❌ Error al conectar con la base de datos:', error.message);
+    }
+}
+
+// Iniciar servidor y probar conexión
+app.listen(PORT, async () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+    await testConnection(); // <------------------- se ejecuta al arrancar el servidor
+});
---
+++ frontend/datos.js
@@ -0,0 +1,33 @@
+const ctx = document.getElementById("grafica");
+
+async function obtenerDatos() {
+    const respuesta = await fetch("http://localhost:3000/api/ventas_producto");
+    return await respuesta.json();
+}
+
+async function generarGrafica() {
+    const datos = await obtenerDatos();
+
+    const labels = datos.map(item => item.categoria);
+
+    // AQUÍ ESTABA EL ERROR
+    const cantidades = datos.map(item => Number(item.cantidad_vendida));
+
+    new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    label: "Ventas Totales",
+                    data: cantidades,
+                    backgroundColor: "rgba(54, 162, 235, 0.5)",
+                    borderColor: "rgba(54, 162, 235, 1)",
+                    borderWidth: 1
+                }
+            ]
+        }
+    });
+}
+
+generarGrafica();

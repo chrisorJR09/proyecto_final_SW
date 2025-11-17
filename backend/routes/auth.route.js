63c671f4c4690e707cc3777a5f84abadd60ba056
Christopher Jauregui <chris.jauran1@gmail.com>
2025-11-17 16:32:53 -0600

    Se agrega ruta para añadir usuario

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,13 @@
 const express=require("express");
 const router=express.Router();
 
-const {login}= require("../controllers/auth.controller");
+const {login, newUser}= require("../controllers/auth.controller");
 
 
 //ruta para el login
 router.post("/login", login);
+
+//ruta para añadir usuarios
+router.post("/newUser", newUser);
 
 module.exports = router;

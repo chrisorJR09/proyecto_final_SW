fd33e3506609401e6e419ef48fb81fadd61368bb
Christopher Jauregui <chris.jauran1@gmail.com>
2025-11-17 20:02:02 -0600

    se agrega ruta para mandar el captcha al frontend

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const router=express.Router();
 
-const {login, newUser}= require("../controllers/auth.controller");
+const {login, newUser, genCaptcha}= require("../controllers/auth.controller");
 
 
 //ruta para el login
@@ -9,5 +9,7 @@ router.post("/login", login);
 
 //ruta para añadir usuarios
 router.post("/newUser", newUser);
+
+router.get('/captcha', genCaptcha);
 
 module.exports = router;
